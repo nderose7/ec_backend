6abab3fa1a238c214cc892f8bdbd4f592d507317
Nick DeRose <nderose7@gmail.com>
2023-11-21 16:12:31 -0500

    CORS for api

--- config/middlewares.js
+++ config/middlewares.js
@@ -14,7 +14,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['https://www.eatclassy.com'], 
+      origin: ['https://www.eatclassy.com', 'https://api.eatclassy.com'], 
     },
   },
   'strapi::poweredBy',
